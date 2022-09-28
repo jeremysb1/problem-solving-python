a98e1faf53b3f6c68f5e5c918e4737e17f56bc8f
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-09-28 06:48:09 -0400

    fixed code aliginment error

--- small problems/compression/compression.py
+++ small problems/compression/compression.py
@@ -2,18 +2,18 @@ class CompressedGene:
     def __init__(self, gene: str) -> None:
         self._compress(gene)
 
-        def _compress(self, gene: str) -> None:
-            self.bit_string: int = 1
-            for nucleotide in gene.upper():
-                self.bit_string <<= 2
-                if nucleotide == "A":
-                    self.bit_string |= 0b00
-                elif nucleotide == "C":
-                    self.bit_string |= 0b01
-                elif nucleotide == "G":
-                    self.bit_string |= 0b10
-                elif nucleotide == "T":
-                    self.bit_string |= 0b11
-                else:
-                    raise ValueError("Invalid Nucleotide: {}".format(nucleotide))
+    def _compress(self, gene: str) -> None:
+        self.bit_string: int = 1
+        for nucleotide in gene.upper():
+            self.bit_string <<= 2
+            if nucleotide == "A":
+                self.bit_string |= 0b00
+            elif nucleotide == "C":
+                self.bit_string |= 0b01
+            elif nucleotide == "G":
+                self.bit_string |= 0b10
+            elif nucleotide == "T":
+                self.bit_string |= 0b11
+            else:
+                raise ValueError("Invalid Nucleotide: {}".format(nucleotide))
                 
