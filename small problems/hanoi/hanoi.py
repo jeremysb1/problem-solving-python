366a82c1c4b3d14d1227de4e04662b23749902ea
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-09-28 21:01:23 -0400

    added hanoi function with recursive algorithm

--- small problems/hanoi/hanoi.py
+++ small problems/hanoi/hanoi.py
@@ -22,3 +22,11 @@ tower_c: Stack[int] = Stack()
 for i in range(1, num_discs + 1):
     tower_a.push(i)
 
+def hanoi(begin: Stack[int], end: Stack[int], temp: Stack[int], n: int) -> None:
+    if n == 1:
+        end.push(begin.pop())
+    else: 
+        hanoi(begin, temp, end, n-1)
+        hanoi(begin, end, temp, 1)
+        hanoi(temp, end, begin, n-1)
+
