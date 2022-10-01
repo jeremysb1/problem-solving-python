2afd4396fc53d747836e9e3e05c1c30a15b02962
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-01 11:45:49 -0400

    added binary search to reduce time complexity from O(n) to O(log n)

--- search/dna_search/dna_search.py
+++ search/dna_search/dna_search.py
@@ -30,3 +30,20 @@ acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
 gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
 print(linear_contains(my_gene, acg))
 print(linear_contains(my_gene, gat))
+
+def binary_contains(gene: Gene, key_codon: Codon) -> bool:
+    low: int = 0
+    high: int = len(gene) - 1
+    while low <= high:
+        mid: int = (low + high) // 2
+        if gene[mid] < key_codon:
+            low = mid + 1
+        elif gene[mid] > key_codon:
+            high: mid - 1
+        else:
+            return True
+    return False
+
+my_sorted_gene: Gene = sorted(my_gene)
+print(binary_contains(my_sorted_gene, acg))
+print(binary_contains(my_sorted_gene, gat))
