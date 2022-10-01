88ad772d173773ac3d9fa063cf555ee8a5409f27
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-01 11:01:46 -0400

    declaring codon as a tuple and gene as a list of codons

--- search/dna_search.py
+++ search/dna_search.py
@@ -2,3 +2,7 @@ from enum import IntEnum
 from typing import Tuple, List
 
 Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]  #type alias for Codon
+Gene = List[Codon] #type alias for genes
+
