6e3f37ce5006cf7c6357638d3341e7e5419ae9ea
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-01 17:28:14 -0400

    fixed syntax errors

--- search/maze/maze.py
+++ search/maze/maze.py
@@ -1,9 +1,9 @@
 from enum import Enum
-from tkinter.tix import CELL
 from typing import List, NamedTuple, Callable, Optional
 import random
 from math import sqrt
 from generic_search import dfs, bfs, node_to_path, astar, Node
+
 
 class Cell(str, Enum):
     EMPTY = " "
@@ -12,28 +12,26 @@ class Cell(str, Enum):
     GOAL = "G"
     PATH = "*"
 
+
 class MazeLocation(NamedTuple):
     row: int
     column: int
 
+
 class Maze:
     def __init__(self, rows: int = 10, columns: int = 10, sparseness: float = 0.2, start: MazeLocation = MazeLocation(0, 0), goal: MazeLocation = MazeLocation(9, 9)) -> None:
-       
         # initialize basic instance variables
         self._rows: int = rows
         self._columns: int = columns
         self.start: MazeLocation = start
         self.goal: MazeLocation = goal
-
         # fill the grid with empty cells
         self._grid: List[List[Cell]] = [[Cell.EMPTY for c in range(columns)] for r in range(rows)]
-
         # populate the grid with blocked cells
         self._randomly_fill(rows, columns, sparseness)
-
         # fill the start and goal locations in
         self._grid[start.row][start.column] = Cell.START
-        self._columns[start.row][start.column] = Cell.GOAL
+        self._grid[goal.row][goal.column] = Cell.GOAL
 
     def _randomly_fill(self, rows: int, columns: int, sparseness: float):
         for row in range(rows):
@@ -47,28 +45,28 @@ class Maze:
         for row in self._grid:
             output += "".join([c.value for c in row]) + "\n"
         return output
-    
+
     def goal_test(self, ml: MazeLocation) -> bool:
         return ml == self.goal
-    
-    def succesors(self, ml: MazeLocation) -> List[MazeLocation]:
+
+    def successors(self, ml: MazeLocation) -> List[MazeLocation]:
         locations: List[MazeLocation] = []
-        if ml.row + 1 < self._rows and self._grid[ml.row][ml.column] != Cell.BLOCKED:
+        if ml.row + 1 < self._rows and self._grid[ml.row + 1][ml.column] != Cell.BLOCKED:
             locations.append(MazeLocation(ml.row + 1, ml.column))
         if ml.row - 1 >= 0 and self._grid[ml.row - 1][ml.column] != Cell.BLOCKED:
             locations.append(MazeLocation(ml.row - 1, ml.column))
         if ml.column + 1 < self._columns and self._grid[ml.row][ml.column + 1] != Cell.BLOCKED:
             locations.append(MazeLocation(ml.row, ml.column + 1))
-        if ml.column - 1 >= 0 and self._grid[ml.row[ml.column - 1]] != Cell.BLOCKED:
-            locations.append(MazeLocation(ml.row, ml.columns - 1))
+        if ml.column - 1 >= 0 and self._grid[ml.row][ml.column - 1] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row, ml.column - 1))
         return locations
-    
+
     def mark(self, path: List[MazeLocation]):
         for maze_location in path:
-            self._grid[maze_location.row][maze_location.column] = Cell.EMPTY
-            self._grid[self.start.row][self.start.column] = Cell.START
-            self._grid[self.goal.row][self.goal.column] = Cell.GOAL
-
+            self._grid[maze_location.row][maze_location.column] = Cell.PATH
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
+    
     def clear(self, path: List[MazeLocation]):
         for maze_location in path:
             self._grid[maze_location.row][maze_location.column] = Cell.EMPTY
@@ -79,8 +77,7 @@ if __name__ == "__main__":
     # Test DFS
     m: Maze = Maze()
     print(m)
-    solution1: Optional[Node[MazeLocation]] = dfs(m.start, m.goal_test,
-     m.successors)
+    solution1: Optional[Node[MazeLocation]] = dfs(m.start, m.goal_test, m.successors)
     if solution1 is None:
         print("No solution found using depth-first search!")
     else:
