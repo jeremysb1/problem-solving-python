67abb2af80ab281eb9c3cc0d8b87c8ca807a4f84
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-01 11:08:52 -0400

    adding utility function to convert a string to a gene

--- search/dna_search.py
+++ search/dna_search.py
@@ -6,3 +6,14 @@ Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]  #type alias for Codon
 Gene = List[Codon] #type alias for genes
 
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
+def gene_to_str(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i + 2) >= len(s):
+            return gene
+        # initialize codon out of three nucleotides
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
+        gene.append(codon)  # add codon to gene
+    return gene
