a6d7b38b579518f36ede8458696b44eac6187dd1
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-01 13:49:54 -0400

    created maze class

--- search/maze/maze.py
+++ search/maze/maze.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from tkinter.tix import CELL
 from typing import List, NamedTuple, Callable, Optional
 import random
 from math import sqrt
@@ -15,3 +16,31 @@ class MazeLocation(NamedTuple):
     row: int
     column: int
 
+class Maze:
+    def __init__(self, rows: int = 10, columns: int = 10, sparseness: float = 0.2, start: MazeLocation = MazeLocation(0, 0), goal: MazeLocation = MazeLocation(9, 9)) -> None:
+       
+        # initialize basic instance variables
+        self._rows: int = rows
+        self._columns: int = columns
+        self.start: MazeLocation = start
+        self.goal: MazeLocation = goal
+
+        # fill the grid with empty cells
+        self._grid: List[List[Cell]] = [[Cell.EMPTY for c in range(columns)] for r in range(rows)]
+
+        # populate the grid with blocked cells
+        self._randomly_fill(rows, columns, sparseness)
+
+        # fill the start and goal locations in
+        self._grid[start.row][start.column] = Cell.START
+        self._columns[start.row][start.column] = Cell.GOAL
+
+    def _randomly_fill(self, rows: int, columns: int, sparseness: float):
+        for row in range(rows):
+            for column in range(columns):
+                if random.uniform(0, 1.0) < sparseness:
+                    self._grid[row][column] = Cell.BLOCKED
+
+    # return a nicely formatted version of the maze for printing
+    def __str__(self) -> str:
+        pass
