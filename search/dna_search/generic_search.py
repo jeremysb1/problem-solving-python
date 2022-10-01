bdc6d2bfbf97f284c0eae21387aee22080b6a3f0
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-01 13:00:19 -0400

    generalizing linear_contains search function and adding class Comparable

---
+++ search/dna_search/generic_search.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typing import Any, TypeVar, Iterable, Sequence, Generic, List, Callable, Set, 
+    Deque, Dict, Any, Optional
+from typing_extensions import Protocol
+from heapq import heappush, heappop
+
+T = TypeVar('T')
+def linear_contains(iterable: Iterable[T], key: T) -> bool:
+    for item in iterable:
+        if item == key:
+            return True
+    return False
+
+C = TypeVar("C", bound="Comparable")
+
+class Comparable(Protocol):
+    def __eq__(self, other: Any) -> bool:
+        ...
+    
+    def __lt__(self: C, other: C) -> bool:
+        ...
+
+    def __gt__(self: C, other: C) -> bool:
+        return (not self < other) and self != other
+
+    def __le__(self: C, other: C) -> bool:
+        return self < other or self == other
+
+    def __ge__(self: C, other: C) -> bool:
+        return not self < other
+
