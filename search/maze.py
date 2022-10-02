7d16a04ee10f6a7030cd658b0ffc573a4b2da6d2
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-02 12:41:05 -0400

    removed empty spaces between functions

--- search/maze.py
+++ search/maze.py
@@ -4,7 +4,6 @@ import random
 from math import sqrt
 from generic_search import dfs, bfs, node_to_path, astar, Node
 
-
 class Cell(str, Enum):
     EMPTY = " "
     BLOCKED = "X"
@@ -12,11 +11,9 @@ class Cell(str, Enum):
     GOAL = "G"
     PATH = "*"
 
-
 class MazeLocation(NamedTuple):
     row: int
     column: int
-
 
 class Maze:
     def __init__(self, rows: int = 10, columns: int = 10, sparseness: float = 0.2, start: MazeLocation = MazeLocation(0, 0), goal: MazeLocation = MazeLocation(9, 9)) -> None:
