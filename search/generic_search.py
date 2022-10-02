4e336cb5d9a5ea3b99d144868dfe80600b4825d9
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-02 14:52:45 -0400

    created astar fucntion to implement A* algorithm

--- search/generic_search.py
+++ search/generic_search.py
@@ -159,6 +159,30 @@ class PriorityQueue(Generic[T]):
     def __repr__(self) -> str:
         return repr(self._container)
 
+def astar(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]], heuristic: Callable[[T], float]) -> Optional[Node[T]]:
+    # frontier is where we've yet to go
+    frontier: PriorityQueue[Node[T]] = PriorityQueue()
+    frontier.push(Node(initial, None, 0.0, heuristic(initial)))
+    # explored is where we've been
+    explored: Dict[T, float] = {initial: 0.0}
+
+    #keep going while there is more to explore
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        #if we found the goal we are done
+        if goal_test(current_state):
+            return current_node
+        # check where we can go next and haven't explored
+        for child in successors(current_state):
+            new_cost: current_nodec.st + 1  # 1 assumes a grid, need a cost function for more sophisticated apps
+
+            if child not in explored or explored[child] > new_cost:
+                explored[cost] = new_cost
+                frontier.push(Node(child, current_node, new_cost, heuristic(child)))
+    
+    return None  # went through everything and never found goal
+
 if __name__ == "__main__":
     print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5))  # True
     print(binary_contains(["a", "d", "e", "f", "z"], "f"))  # True
