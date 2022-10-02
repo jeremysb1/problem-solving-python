200b82a42df3344037a432c17cf74f21d7a9311b
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-02 16:40:22 -0400

    created csp class as a gathering point for variables, domains, and constraints

--- constraint-satisfaction/csp.py
+++ constraint-satisfaction/csp.py
@@ -18,3 +18,14 @@ class Constraint(Generic[V, D], ABC):
 # A constraint satisfaction problem consists of variables of type V
 # that have ranges of values known as domains of type D and constraints
 # that determine whether a particular variable's domain selection is valid
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
+        self.variables: List[V] = variables # variables to be constrained
+        self.domains: Dict[V, List[D]] = domains # domain of each variable
+        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError("Every variable should have a domain assigned to it.")
+        
+        
