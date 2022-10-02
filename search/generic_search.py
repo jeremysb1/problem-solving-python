4b2415f0d5bc9f203e50f6e7e6546e8588c5e89f
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-02 14:56:42 -0400

    fixed syntax errors

--- search/generic_search.py
+++ search/generic_search.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
-from audioop import add
-from queue import Empty
 from typing import TypeVar, Iterable, Sequence, Generic, List, Callable, Set, Deque, Dict, Any, Optional
 from typing_extensions import Protocol
 from heapq import heappush, heappop
 
 T = TypeVar('T')
+
 
 def linear_contains(iterable: Iterable[T], key: T) -> bool:
     for item in iterable:
@@ -15,6 +14,7 @@ def linear_contains(iterable: Iterable[T], key: T) -> bool:
 
 
 C = TypeVar("C", bound="Comparable")
+
 
 class Comparable(Protocol):
     def __eq__(self, other: Any) -> bool:
@@ -32,6 +32,7 @@ class Comparable(Protocol):
     def __ge__(self: C, other: C) -> bool:
         return not self < other
 
+
 def binary_contains(sequence: Sequence[C], key: C) -> bool:
     low: int = 0
     high: int = len(sequence) - 1
@@ -45,22 +46,24 @@ def binary_contains(sequence: Sequence[C], key: C) -> bool:
             return True
     return False
 
+
 class Stack(Generic[T]):
     def __init__(self) -> None:
         self._container: List[T] = []
-    
+
     @property
     def empty(self) -> bool:
-        return not self._container # not is true for empty container
-    
+        return not self._container  # not is true for empty container
+
     def push(self, item: T) -> None:
         self._container.append(item)
 
     def pop(self) -> T:
         return self._container.pop()  # LIFO
-    
+
     def __repr__(self) -> str:
         return repr(self._container)
+
 
 class Node(Generic[T]):
     def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0) -> None:
@@ -70,7 +73,8 @@ class Node(Generic[T]):
         self.heuristic: float = heuristic
 
     def __lt__(self, other: Node) -> bool:
-        return (self.cost + self.heuristic) < (other.cost < other.heuristic)
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+
 
 def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
     # frontier is where we've yet to go
@@ -94,6 +98,7 @@ def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], Li
             frontier.push(Node(child, current_node))
     return None  # went through everything and never found goal
 
+
 def node_to_path(node: Node[T]) -> List[T]:
     path: List[T] = [node.state]
     # work backwards from end to front
@@ -103,61 +108,65 @@ def node_to_path(node: Node[T]) -> List[T]:
     path.reverse()
     return path
 
+
 class Queue(Generic[T]):
     def __init__(self) -> None:
         self._container: Deque[T] = Deque()
-    
+
     @property
     def empty(self) -> bool:
         return not self._container  # not is true for empty container
-    
+
     def push(self, item: T) -> None:
         self._container.append(item)
-    
+
     def pop(self) -> T:
-        return self._container.popleft()   #FIFO
-    
+        return self._container.popleft()  # FIFO
+
     def __repr__(self) -> str:
         return repr(self._container)
+
 
 def bfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
     # frontier is where we've yet to go
     frontier: Queue[Node[T]] = Queue()
     frontier.push(Node(initial, None))
-    #explored is where we've been
+    # explored is where we've been
     explored: Set[T] = {initial}
 
-    #keep going is where we need to explore
+    # keep going while there is more to explore
     while not frontier.empty:
         current_node: Node[T] = frontier.pop()
         current_state: T = current_node.state
-        # if we found the goal we are done
+        # if we found the goal, we're done
         if goal_test(current_state):
             return current_node
         # check where we can go next and haven't explored
         for child in successors(current_state):
-            if child in explored:  #skip children we have already explored
+            if child in explored:  # skip children we already explored
                 continue
             explored.add(child)
             frontier.push(Node(child, current_node))
     return None  # went through everything and never found goal
 
+
 class PriorityQueue(Generic[T]):
     def __init__(self) -> None:
-        self._container: List[T] = [T]
-    
+        self._container: List[T] = []
+
     @property
     def empty(self) -> bool:
         return not self._container  # not is true for empty container
-    
+
     def push(self, item: T) -> None:
-        heappush(self._container, item) # in by property
-    
+        heappush(self._container, item)  # in by priority
+
     def pop(self) -> T:
         return heappop(self._container)  # out by priority
-    
+
     def __repr__(self) -> str:
         return repr(self._container)
+
 
 def astar(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]], heuristic: Callable[[T], float]) -> Optional[Node[T]]:
     # frontier is where we've yet to go
@@ -182,7 +191,8 @@ def astar(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], 
                 frontier.push(Node(child, current_node, new_cost, heuristic(child)))
     return None  # went through everything and never found goal
 
+
 if __name__ == "__main__":
     print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5))  # True
     print(binary_contains(["a", "d", "e", "f", "z"], "f"))  # True
-    print(binary_contains(["john", "mark", "ronald", "sarah"], "sheila"))  #False
+    print(binary_contains(["john", "mark", "ronald", "sarah"], "sheila"))  # False
