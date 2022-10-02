f424a9392202ef329a2741b5913b6c40c86c858e
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-02 18:12:02 -0400

    added constraint and ran search to test solution

--- constraint-satisfaction/queens.py
+++ constraint-satisfaction/queens.py
@@ -19,3 +19,15 @@ class QueensConstraint(Constraint[int, int]):
                         return False
         return True # no conflict
 
+if __name__ == "__main__":
+    columns: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+    rows: Dict[int, List[int]] = {}
+    for column in columns:
+        rows[column] = [1, 2, 3, 4, 5, 6, 7, 8]
+    csp: CSP[int, int] = CSP(columns, rows)
+    csp.add_constraint(QueensConstraint(columns))
+    solution: Optional[Dict[int, int]] = csp.backtracking_search()
+    if solution is None:
+        print("No solution found!")
+    else:
+        print(solution)
