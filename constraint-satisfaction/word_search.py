09c978d633d7365b661388b60a22ce15c096cf37
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-03 19:47:30 -0400

    testing solution

--- constraint-satisfaction/word_search.py
+++ constraint-satisfaction/word_search.py
@@ -51,4 +51,23 @@ class WordSearchConstraint(Constraint[str, List[GridLocation]]):
         all_locations = [locs for values in assigment.values() for locs in values]
         return len(set(all_locations)) == len(all_locations)
 
-
+if __name__ == "__main__":
+    grid: Grid = generate_grid(9, 9)
+    words: List[str] = ["MATTHEW", "JOE", "MARY", "SARAH", "SALLY"]
+    locations: Dict[str, List[List[GridLocation]]] = {}
+    for word in words:
+        locations[word] = generate_domain(word, grid)
+    csp: CSP[str, List[GridLocation]] = CSP(words, locations)
+    csp.add_constraint(WordSearchConstraint(words))
+    solution: Optional[Dict[str, List[GridLocation]]] = csp.backtracking_search()
+    if solution is None:
+        print("No solution found!")
+    else:
+        for word, grid_locations in solution.items():
+            # random reverse half the time
+            if choice([True, False]):
+                grid_locations.reverse()
+            for index, letter in enumerate(word):
+                (row, col) = (grid_locations[index].row, grid_locations[index].column)
+                grid[row][col] = letter
+        display_grid(grid)
