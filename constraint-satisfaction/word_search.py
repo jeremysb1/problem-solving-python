75d3f11a037255542f7723bd46e373fae12240a4
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-03 19:31:44 -0400

    created generate_domain function to figure out where words fit on grid

--- constraint-satisfaction/word_search.py
+++ constraint-satisfaction/word_search.py
@@ -17,3 +17,26 @@ def display_grid(grid: Grid) -> None:
     for row in grid:
         print("".join(row))
 
+def generate_domain(word: str, grid: Grid) -> List[List[GridLocation]]:
+    domain: List[List[GridLocation]] = []
+    height: int = len(grid)
+    width: int = len(grid[0])
+    length: int = len(word)
+    for row in range(height):
+        for col in range(width):
+            columns: range = range(col, col + length + 1)
+            rows: range = range(row, row + length + 1)
+            if col + length <= width:
+                # left to right
+                domain.append([GridLocation(row, c) for c in columns])
+                # diagonal towards bottom right
+                if row + length <= height:
+                    domain.append([GridLocation(r, col + (r - row)) for r in rows])
+                
+                if row + length <= height:
+                    # top to bottom
+                    domain.append([GridLocation(r, col) for r in rows])
+                    # diagonal towards bottom left
+                    if col - length >= 0:
+                        domain.append([GridLocation(r, col - (r - row)) for r in rows])
+    return domain
