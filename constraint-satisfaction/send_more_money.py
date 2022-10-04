4c4350bf67eb99fb88bb4423d171eef29dfe240c
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-04 19:47:12 -0400

    creating class SendMoreMoneyConstraint

---
+++ constraint-satisfaction/send_more_money.py
@@ -0,0 +1,9 @@
+from csp import Constraint, CSP
+from typing import Dict, List, Optional
+
+class SendMoreMoneyConstraint(Constraint[str, int]):
+    def __init__(self, letters: List[str]) -> None:
+        super().__init__(letters)
+        self.letters: List[str] = letters
+
+    
