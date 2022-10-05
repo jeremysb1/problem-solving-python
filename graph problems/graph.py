30f9db95d3d94451cf1b4cbfc6328c3d82318f5d
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-05 19:31:09 -0400

    created add_vertex method

--- graph problems/graph.py
+++ graph problems/graph.py
@@ -8,3 +8,19 @@ class Graph(Generic[V]):
     def __init__(self, vertices: List[V] = []) -> None:
         self._vertices: List[V] = vertices
         self._edges: List[List[Edge]] = [[] for _ in vertices]
+
+    @property
+    def vertex_count(self) -> int:
+        return len(self._vertices)  # Number of vertices
+
+    @property
+    def edge_count(self) -> int:
+        return sum(map(len, self._edges))  # Number of edges
+
+    # Add a vertex to the graph and return its index
+    def add_vertex(self, vertex: V) -> int:
+        self._vertices.append(vertex)
+        self._edges.append([])  # add empty list for containing edges
+        return self.vertex_count - 1  # return index of added vertex
+
+    
