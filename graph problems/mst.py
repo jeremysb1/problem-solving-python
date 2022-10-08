bdf7f5582182472c0a274b56ee705f5b9720a436
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-08 10:58:03 -0400

    created mst function to implement Jarnik's algorithm

--- graph problems/mst.py
+++ graph problems/mst.py
@@ -8,3 +8,29 @@ WeightedPath = List[WeightedEdge]
 
 def total_weight(wp: WeightedPath) -> float:
     return sum([e.weight for e in wp])
+
+def mst(wg: WeightedGraph[V], start: 0) -> Optional[WeightedPath]:
+    if start > (wg.vertex_count - 1) or start < 0:
+        return None
+    result: WeightedPath = []  # holds the final MST
+    pq: PriorityQueue[WeightedEdge] = PriorityQueue()
+    visited: List[bool] = [False] * wg.vertex_count
+
+    def visit(index: int):
+        visited[index] = True  #mark as visited
+        for edge in wg.edges_for_index(index):
+            # add all edges coming from here to pq
+            if not visited[edge.v]:
+                pq.push(edge)
+        
+    visit(start)  # the first vertex is where everything begins
+
+    while not pq.empty: # keep going while there are edges to process
+        edge = pq.pop()
+        if visited[edge.v]:
+            continue # don't ever revisit
+        # this is the current smallest, so add it to solution
+        result.append(edge)
+        visit(edge.v)  # visit where this connects
+    
+    return result
