15d7d25c8fe96ab362863bdab79c88b648fd2941
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-08 07:32:24 -0400

    added methods to sort edges by weight

--- graph problems/weighted_edge.py
+++ graph problems/weighted_edge.py
@@ -8,3 +8,12 @@ class WeightedEdge(Edge):
 
     def reversed(self) -> WeightedEdge:
         return WeightedEdge(self.v, self.u, self.weight)
+    
+    # so that we can order edges by weight to find the minimum weight edge
+    def __lt__(self, other: WeightedEdge) -> bool:
+        return self.weight < other.weight
+    
+    def __str__(self) -> str:
+        return f"{self.u} {self.weight} > {self.v}"
+    
+    
