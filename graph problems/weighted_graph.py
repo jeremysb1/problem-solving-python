d68db9d3b1dab544a385abe4cba6e2dd179270e7
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-08 07:52:53 -0400

    adding constructor method

--- graph problems/weighted_graph.py
+++ graph problems/weighted_graph.py
@@ -5,4 +5,8 @@ from weighted_edge import WeightedEdge
 V = TypeVar('V')  # type of the vertices in the graph
 
 class WeightedGraph(Generic[V], Graph[V]):
-    
+    def __init__(self, vertices: List[V] = []) -> None:
+        self._vertices: List[V] = vertices
+        self._edges: List[List[WeightedEdge]] = [[] for _ in vertices]
+
+        
