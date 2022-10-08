342199ae09f39836e877ba229f168b22477a564e
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-08 11:09:37 -0400

    added utility function to print WeightedPath

--- graph problems/mst.py
+++ graph problems/mst.py
@@ -34,3 +34,8 @@ def mst(wg: WeightedGraph[V], start: 0) -> Optional[WeightedPath]:
         visit(edge.v)  # visit where this connects
     
     return result
+
+def print_weighted_path(wg: WeightedGraph, wp: WeightedPath) -> None:
+    for edge in wp:
+        print(f"{wg.vertex_at(edge.u)} {edge.weight} > {wg.vertex_at(edge.v)}")
+    print(f"Total Weight: {total_weight(wp)}")
