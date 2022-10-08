74c56114a9d33712b1d4d32a72dc3cd05fd29d8a
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-08 14:55:28 -0400

    adding dijkstra file and created DijkstraNode Class

---
+++ graph problems/dijkstra.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from typing import TypeVar, List, Optional, Tuple, Dict
+from dataclasses import dataclass
+from mst import WeightedPath, print_weighted_path
+from weighted_graph import WeightedGraph
+from weighted_edge import WeightedEdge
+from priority_queue import PriorityQueue
+
+V = TypeVar('V')  # type of the vertices in the graph
+
+@dataclass
+class DijkstraNode:
+    vertex: int
+    distance: float
+
+    def __lt__(self, other: DijkstraNode) -> bool:
+        return self.distance < other.distance
+
+    def __eq__(self, other: DijkstraNode) -> bool:
+        return self.distance == other.distance
+
