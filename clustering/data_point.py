59871a06d65ad05c6379d660101923738104d3ab
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-10 18:44:52 -0400

    created DataPoint class

---
+++ clustering/data_point.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from typing import Iterator, Tuple, List, Iterable
+from math import sqrt
+
+class DataPoint:
+    def __init__(self, initial: Iterable[float]) -> None:
+        self._originals: Tuple[float, ...] = Tuple(initial)
+        self.dimensions: Tuple[float, ...] = Tuple(initial)
+
+    
