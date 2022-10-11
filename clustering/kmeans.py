d2d319e6758599c1e25a17626f3564dd03c13ea7
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-11 06:36:06 -0400

    added _zscore_normalize method

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -40,3 +40,12 @@ class KMeans(Generic[Point]):
     
     def _dimension_slice(self, dimension: int) -> List[float]:
         return [x.dimensions[dimension] for x in self._points]
+
+    def _zscore_normalize(self) -> None:
+        zscored: List[List[float]] = [[] for _ in range(len(self._points))]
+        for dimension in range(self._points[0].num_dimensions):
+            dimension_slice: List[float] = self._dimension_slice(dimension)
+            for index, zscore in enumerate(zscores(dimension_slice)):
+                zscored[index].append(zscore)
+        for i in range(len(self._points)):
+            self._points[i].dimensions = tuple(zscored[i])
