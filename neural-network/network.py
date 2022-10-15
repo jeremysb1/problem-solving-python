bab77d11f52a818b5250ddc1fcdab5a3f01ccbef
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-15 09:20:46 -0400

    created Network class

--- neural-network/network.py
+++ neural-network/network.py
@@ -1,7 +1,21 @@
 from __future__ import annotations
+from termios import N_TTY
 from typing import List, Callable, TypeVar, Tuple
 from functools import reduce
 from layer import Layer
 from util import sigmoid, derivative_sigmoid
 
 T = TypeVar('T')  # output type of interpretation of neural network
+
+class Network:
+    def __init__(self, layer_structure: List[int], learning_rate: float, activation_function: Callable[[float], float] = sigmoid, derivative_activation_function: Callable[[float], float] = derivative_sigmoid) -> None:
+        if len(layer_structure) < 3:
+            raise ValueError("Error: Should be at least 3 layers (1 input, 1 hidden, 1 output)")
+        self.layers: List[Layer] = []
+        # input layer
+        input_layer: Layer = Layer(None, layer_structure[0], learning_rate, activation_function, derivative_activation_function)
+        self.layers.append(input_layer)
+        # hidden layers and output layer
+        for previous, num_neurons in enumerate(layer_structure[1::]):
+            next_layer = Layer(self.layers[previous], num_neurons, learning_rate, activation_function, derivative_activation_function)
+            self.layers.append(next_layer)
