e5f645b022de9b2b8cda2af7f8fc5064961493b0
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-16 14:25:42 -0400

    preparing data set for training and testing

--- neural-network/wine_test.py
+++ neural-network/wine_test.py
@@ -10,3 +10,16 @@ if __name__ == "__main__":
     wine_species: List[int] = []
     with open('wine.csv', mode='r') as wine_file:
         wines: List = list(csv.reader(wine_file, quoting=csv.QUOTE_NONNUMERIC))
+        shuffle(wines)
+        for wine in wines:
+            parameters: List[float] = [float(n) for n in wine[1:14]]
+            wine_parameters.append(parameters)
+            species: int = int(wine[0])
+            if species == 1:
+                wine_classifications.append([1.0, 0.0, 0.0])
+            elif species == 2:
+                wine_classifications.append([0.0, 1.0, 0.0])
+            else:
+                wine_classifications.append([0.0, 0.0, 1.0])
+            wine_species.append(species)
+    normalize_by_feature_scaling(wine_parameters)
