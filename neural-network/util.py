88997023d1e5c24e228d78886431b3792badc0d2
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-16 09:39:42 -0400

    added feature scaling function

--- neural-network/util.py
+++ neural-network/util.py
@@ -11,3 +11,13 @@ def sigmoid(x: float) -> float:
 def derivative_sigmoid(x: float) -> float:
     sig: float = sigmoid(x)
     return sig * (1 - sig)
+
+# assume all rows are of equal length
+# and feature scale each column to be in the range 0 - 1
+def normalize_by_feature_scaling(dataset: List[List[float]]) -> None:
+    for col_num in range(len(dataset[0])):
+        column: List[float] = [row[col_num] for row in dataset]
+        maximum = max(column)
+    minimum = min(column)
+    for row_num in range(len(dataset)):
+        dataset[row_num][col_num] = (dataset[row_num][col_num] - minimum) / (maximum - minimum)
