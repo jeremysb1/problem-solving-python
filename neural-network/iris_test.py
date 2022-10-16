3a84208960ae7c216034af56a7f8934739b621c7
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-16 11:01:50 -0400

    added iris_test file and imported data for classification

---
+++ neural-network/iris_test.py
@@ -0,0 +1,25 @@
+import csv
+from typing import List
+from symbol import parameters
+from util import normalize_by_feature_scaling
+from network import Network
+from random import shuffle
+
+if __name__ == "__main__":
+    iris_parameters: List[List[float]] = []
+    iris_classifications: List[List[float]] = []
+    iris_species: List[str] = []
+    with open('iris.csv', mode='r') as iris_file:
+        irises: List = list(csv.reader(iris_file))
+        shuffle(irises)
+        for iris in irises:
+            parameters: List[float] = [float(n) for n in iris[0:4]]
+            iris_parameters.append(parameters)
+            species: str = iris[4]
+            if species == "Iris-setosa":
+                iris_classifications.append([1.0, 0.0, 0.0])
+            elif species == "Iris-versicolor":
+                iris_classifications.append([0.0, 1.0, 0.0])
+            else:
+                iris_classifications.append([0.0, 0.0, 1.0])
+    normalize_by_feature_scaling(iris_parameters)
