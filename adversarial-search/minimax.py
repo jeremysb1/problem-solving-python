8c74387be8d969eff8a5e2fbab82bb516c891c1b
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-20 14:07:01 -0400

    added minimax file and implemented minimax algorithm

---
+++ adversarial-search/minimax.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from board import Piece, Board, Move
+
+def minimax(board: Board, maximizing: bool, original_player: Piece, max_depth: int = 8) -> float:
+    if board.is_win or board.is_draw or max_depth == 0:
+        return board.evaluate(original_player)
+    
+    if maximizing:
+        best_eval: float = float("-inf")
+        for move in board.legal_moves:
+            result: float = minimax(board.move(move), False, original_player, max_depth - 1)
+            best_eval = max(result, best_eval)
+        return best_eval
+    else:
+        worst_eval: float = float("inf")
+        for move in board.legal_moves:
+            result = minimax(board.move(move), True, original_player, max_depth - 1)
+            worst_eval = min(result, worst_eval)
+        return worst_eval
