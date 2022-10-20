b47357d5f410c2e3ede9bf4a841cd6af599832d3
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-20 11:48:53 -0400

    added methods to TTTBoard

--- adversarial-search/tictactoe.py
+++ adversarial-search/tictactoe.py
@@ -29,3 +29,37 @@ class TTTBoard(Board):
     def turn(self) -> Piece:
         return self._piece
     
+    def move(self, location: Move) -> Board:
+        temp_position: List[TTTPiece] = self.position.copy()
+        temp_position[location] = self._turn
+        return TTTBoard(temp_position, self._turn.opposite)
+
+    @property
+    def legal_moves(self) -> List[Move]:
+        return [Move(l) for l in range(len(self.position)) if self.position[l] == TTTPiece.E]
+    
+    @property
+    def is_win(self) -> bool:
+        return self.position[0] == self.position[1] and self.position[0] == self.position[2] and self.position[0] != TTTPiece.E or \
+            self.position[3] == self.position[4] and self.position[3] == self.position[5] and self.position[3] != TTTPiece.E or \
+            self.position[6] == self.position[7] and self.position[6] == self.position[8] and self.position[6] != TTTPiece.E or \
+            self.position[0] == self.position[3] and self.position[0] == self.position[6] and self.position[0] != TTTPiece.E or \
+            self.position[1] == self.position[4] and self.position[1] == self.position[7] and self.position[1] != TTTPiece.E or \
+            self.position[2] == self.position[5] and self.position[2] == self.position[8] and self.position[2] != TTTPiece.E or \
+            self.position[0] == self.position[4] and self.position[0] == self.position[8] and self.position[0] != TTTPiece.E or \
+            self.position[2] == self.position[4] and self.position[2] == self.position[6] and self.position[2] != TTTPiece.E
+    
+    def evaluate(self, player: Piece) -> float:
+        if self.is_win and self.turn == player:
+            return -1
+        elif self.is_win and self.turn != player:
+            return 1
+        else:
+            return 0
+    
+    def __repr__(self) -> str:
+        return f"""{self.position[0]}|{self.position[1]}|{self.position[2]}
+-----
+{self.position[3]}|{self.position[4]}|{self.position[5]}
+-----
+{self.position[6]}|{self.position[7]}|{self.position[8]}"""
