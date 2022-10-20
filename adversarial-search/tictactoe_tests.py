d5d6c57fce656c6363f73b7c47936264a840d182
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-20 15:46:42 -0400

    added methods to TTTMinimaxTestCase class

--- adversarial-search/tictactoe_tests.py
+++ adversarial-search/tictactoe_tests.py
@@ -12,3 +12,19 @@ class TTTMinimaxTestCase(unittest.TestCase):
         test_board1: TTTBoard = TTTBoard(to_win_easy_position, TTTPiece.X)
         answer1: Move = find_best_move(test_board1)
         self.assertEqual(answer1, 6)
+    
+    def test_block_position(self):
+        to_block_position: List[TTTPiece] = [TTTPiece.X, TTTPiece.E, TTTPiece.E,
+                                             TTTPiece.E, TTTPiece.E, TTTPiece.O,
+                                             TTTPiece.E, TTTPiece.X, TTTPiece.O]
+        test_board2: TTTBoard = TTTBoard(to_block_position, TTTPiece.X)
+        answer2: Move = find_best_move(test_board2)
+        self.assertEqual(answer2, 2)
+    
+    def test_hard_position(self):
+        to_win_hard_position: List[TTTPiece] = [TTTPiece.X, TTTPiece.E, TTTPiece.E,
+                                                TTTPiece.E, TTTPiece.E, TTTPiece.O,
+                                                TTTPiece.O, TTTPiece.X, TTTPiece.E]
+        test_board3: TTTBoard = TTTBoard(to_win_hard_position, TTTPiece.X)
+        answer3: Move = find_best_move(test_board3)
+        self.assertEqual(answer3, 1)
