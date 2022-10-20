f3fe3f2076661fd7e5f29746c17bedbb3938b0f3
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-10-20 05:14:15 -0400

    added Piece class

--- adversarial-search/board.py
+++ adversarial-search/board.py
@@ -3,3 +3,8 @@ from typing import NewType, List
 from abc import ABC, abstractmethod
 
 Move = NewType('Move', int)
+
+class Piece:
+    @property
+    def opposite(self) -> Piece:
+        raise NotImplementedError("Should be implemented by subclasses.")
